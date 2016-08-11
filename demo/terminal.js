b1f4c6a095218c4c5d9410435eb92d6c50ff68c6
Rawanalharbi <harbi.rawan@gmail.com>
2016-08-11 18:16:10 -0500

    adding the whole thing

--- demo/terminal.js
+++ demo/terminal.js
@@ -27,7 +27,7 @@ function stopRunning() {
 
 function retrieveSampleImage() {
   var oReq = new XMLHttpRequest();
-  oReq.open("GET", "../bigbuckbunny.jpg", true);
+  oReq.open("GET", "/Users/Rawan/Desktop/testpic.png", true);
   oReq.responseType = "arraybuffer";
 
   oReq.onload = function (oEvent) {
